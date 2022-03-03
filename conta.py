3f8b78dfc0b71e753df72b10e311457e2298921a
Nicholas Barros <81240462+NickRyanDev@users.noreply.github.com>
2022-03-03 10:55:46 -0300

    put types in fuctions

--- conta.py
+++ conta.py
@@ -1,6 +1,6 @@
 class Conta:
 
-    def __init__(self, number, owner, balance, limit):
+    def __init__(self, number: str, owner: str, balance: int, limit: int):
         print(f"Criando conta {self}")
         self.__numero = number
         self.__titular = owner
@@ -11,22 +11,22 @@ class Conta:
         """shows balance and owner of account"""
         print(f"Saldo de {self.__balance} do titular {self.__owner}")
 
-    def deposit(self, value):
+    def deposit(self, value: float) -> float:
         """do deposit of value in an account"""
         self.__balance += value
 
-    def __check_cash_out(self, value_to_cash_out):
+    def __check_cash_out(self, value_to_cash_out: float) -> float:
         """check withdrawal availability"""
         value_to_cash_out = self.__balance + self.__limit
         return valor_a_sacar <= valor_disponivel_a_sacar
 
-    def cash_out(self, value):
+    def cash_out(self, value: float):
         if(self.__check_cash_out(value)):
             self.__balance -= value
         else:
             print(f"O valor {value} passou o limite")
 
-    def transfer(self, valor, destino):
+    def transfer(self, valor: float, destino: str):
         """performs the transfer of values between two accounts"""
         self.cash_out(value)
         destino.deposit(value)
